224ceaa96fce8a2dec3a55d0c7344d3d03acf764
Scott Wadden <scott.wadden@emc.com>
2020-02-26 17:07:39 -0400

    Updated sample and gitignore

--- exposed.nim
+++ exposed.nim
@@ -1,11 +1,12 @@
 proc forward*(steps = 1.0) = discard
 proc back*(steps = 1.0)    = discard
+proc left*(degrees = 90.0) = discard
+proc right*(degrees = 90.0)= discard
 proc fd*(steps = 1.0)      = forward(steps)
 proc bk*(steps = 1.0)      = back(steps)
+proc print*(msg: string)   = discard
 
 template go*(rules) =
   proc run*() =
     rules
   
-  # when not defined(vm):
-  #   build_rules()
--- myscript.nim
+++ myscript.nim
@@ -3,6 +3,5 @@ import exposed
 go:
   while true:
     fd()
-    for x in 0..5: echo x
     fd()
-    back()
+    right()
