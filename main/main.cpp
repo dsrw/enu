e582bb3fd2ac2152d8d82ac1710ecac279f7db3d
Scott Wadden <scott.wadden@mpicorp.ca>
2020-06-28 21:31:50 -0300

    Squashed 'vendor/godot/' changes from 426fffe67b..ae75ba1bf4
    ae75ba1bf4 Merge tag '3.2.2-stable' into textedit-indent-spaces
    6a9fbafcbb Bump version to 3.2.2-stable
    63523e56e1 Don't use arbitrary theme editor icons for scripts with the same name
    c682410379 Mono/C#: Fix unhandled exception not being printed
    c8586d3837 Merge pull request #39840 from neikeq/3.2-use-/restore-instead-of-/t-restore
    705b1695af Merge pull request #39818 from Wavesonics/server-memory-leak
    abc453f9b6 [3.2] Mono/C#: Use /restore instead of /t:restore when building
    23d44223e6 Fix memory leaks in RasterizerStorageDummy::free Lightmap capture data is now freed as well free() now also properly returns true or false based on if something was actually freed.
    5079bea660 Merge pull request #39820 from Chaosus/shader_fix_const_crash_3.2
    76fa1724b3 Merge pull request #39395 from asmaloney/fix-macos-scrollwheel-zoom
    a84f081160 i18n: Sync translations with Weblate
    474b178534 [3.2] Fix shader crash if pass const argument to 'out/inout' parameter
    087a83fd54 Improve the low processor mode sleep precision
    530307e7ec Main::cleanup: Move MessageQueue deletion further down where it's safer
    6dee6477dd Optimize class icon loading
    d4a1bc475a Fix overlapping hotkey designations for save all scenes and save all scripts
    68c273fb09 Tween.xml word order fix
    d2347bc4ec Clarify what a string being empty means
    5be4e10f4e Fix error label clicking in status bar for shader editor
    0f30fa8fbd Enable finer grained control when creating polygon with UV Editor
    944210923f Follow up cleanup for the godotpayment project module
    0867fc2c4c Draw horizontal lines and labels in the editor performance monitors
    afac2514f8 Fix upscaling image with bilinear interpolation option specified
    8f28e0861f Merge pull request #39762 from naithar/feature/ios-modules
    8b210f8732 Merge pull request #39806 from RandomShaper/fix_gone_popup_crash_3.2
    0f0360702b Handle gone TabContainer popup nicely
    1575820207 Merge pull request #39791 from PoqXert/ios
    bd71925726 Updated purchase receipt suitable for sending to apple verification server
    c7bbe262dd Merge pull request #39333 from Aroweeri/environment
    7ec072f7e7 Merge pull request #39774 from lawnjelly/kessel_tex_wrapping_fix
    491059ca82 GLES2 Batching - Fix texture wrapping state bug.
    198bd4cb0f Environment doc update regarding Viewport usage.
    45a6628b15 SCons: Enable iOS modules for ARKit and Camera
    e564ca0c1b Export: Fix iOS enabling push notifications capability
    bc6a75786c Export: Remove system frameworks from xcode template
    a678ceaeae SCons: Enable modules for iOS
    f4640af9c1 [3.2][macOS] Control key + scroll wheel should zoom not pan
    
    git-subtree-dir: vendor/godot
    git-subtree-split: ae75ba1bf49572b0e6fc0f64fa2b9848809863a4

--- main/main.cpp
+++ main/main.cpp
@@ -2163,22 +2163,37 @@ bool Main::iteration() {
 	if (fixed_fps != -1)
 		return exit;
 
-	if (OS::get_singleton()->is_in_low_processor_usage_mode() || !OS::get_singleton()->can_draw())
-		OS::get_singleton()->delay_usec(OS::get_singleton()->get_low_processor_usage_mode_sleep_usec()); //apply some delay to force idle time
-	else {
-		uint32_t frame_delay = Engine::get_singleton()->get_frame_delay();
-		if (frame_delay)
-			OS::get_singleton()->delay_usec(Engine::get_singleton()->get_frame_delay() * 1000);
+	const uint32_t frame_delay = Engine::get_singleton()->get_frame_delay();
+	if (frame_delay) {
+		// Add fixed frame delay to decrease CPU/GPU usage. This doesn't take
+		// the actual frame time into account.
+		// Due to the high fluctuation of the actual sleep duration, it's not recommended
+		// to use this as a FPS limiter.
+		OS::get_singleton()->delay_usec(frame_delay * 1000);
 	}
 
-	int target_fps = Engine::get_singleton()->get_target_fps();
+	// Add a dynamic frame delay to decrease CPU/GPU usage. This takes the
+	// previous frame time into account for a smoother result.
+	uint64_t dynamic_delay = 0;
+	if (OS::get_singleton()->is_in_low_processor_usage_mode() || !OS::get_singleton()->can_draw()) {
+		dynamic_delay = OS::get_singleton()->get_low_processor_usage_mode_sleep_usec();
+	}
+	const int target_fps = Engine::get_singleton()->get_target_fps();
 	if (target_fps > 0 && !Engine::get_singleton()->is_editor_hint()) {
-		uint64_t time_step = 1000000L / target_fps;
-		target_ticks += time_step;
+		// Override the low processor usage mode sleep delay if the target FPS is lower.
+		dynamic_delay = MAX(dynamic_delay, (uint64_t)(1000000 / target_fps));
+	}
+
+	if (dynamic_delay > 0) {
+		target_ticks += dynamic_delay;
 		uint64_t current_ticks = OS::get_singleton()->get_ticks_usec();
-		if (current_ticks < target_ticks) OS::get_singleton()->delay_usec(target_ticks - current_ticks);
+
+		if (current_ticks < target_ticks) {
+			OS::get_singleton()->delay_usec(target_ticks - current_ticks);
+		}
+
 		current_ticks = OS::get_singleton()->get_ticks_usec();
-		target_ticks = MIN(MAX(target_ticks, current_ticks - time_step), current_ticks + time_step);
+		target_ticks = MIN(MAX(target_ticks, current_ticks - dynamic_delay), current_ticks + dynamic_delay);
 	}
 
 #ifdef TOOLS_ENABLED
@@ -2219,8 +2234,8 @@ void Main::cleanup() {
 	ResourceLoader::remove_custom_loaders();
 	ResourceSaver::remove_custom_savers();
 
+	// Flush before uninitializing the scene, but delete the MessageQueue as late as possible.
 	message_queue->flush();
-	memdelete(message_queue);
 
 	if (script_debugger) {
 		if (use_debug_profiler) {
@@ -2296,6 +2311,10 @@ void Main::cleanup() {
 		OS::get_singleton()->execute(exec, args, false, &pid);
 		OS::get_singleton()->set_restart_on_exit(false, List<String>()); //clear list (uses memory)
 	}
+
+	// Now should be safe to delete MessageQueue (famous last words).
+	message_queue->flush();
+	memdelete(message_queue);
 
 	unregister_core_driver_types();
 	unregister_core_types();
