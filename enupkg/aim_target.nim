6829c99bedc62d465e79d7a7fe68dda770d289b4
Scott Wadden <dsrw@me.com>
2020-10-26 15:15:27 -0300

    Aim target supports scaled/rotated objects

--- enupkg/aim_target.nim
+++ enupkg/aim_target.nim
@@ -1,11 +1,12 @@
 import ../godotapi / [sprite_3d, ray_cast, spatial],
        godot,
-       strutils,
+       strutils, math,
        globals, core
 
 gdobj AimTarget of Sprite3D:
   var
-    last_collider: Object
+    last_collider: Spatial
+    last_point, last_normal: Vector3
 
   method ready*() =
     trace:
@@ -16,13 +17,18 @@ gdobj AimTarget of Sprite3D:
     if self.last_collider != nil:
       self.last_collider.trigger("target_out")
       self.last_collider = nil
+      self.last_point = vec3()
+      self.last_normal = vec3()
 
   method on_hide_target() = self.visible = false
   method on_show_target() = self.visible = true
 
   proc update*(ray: RayCast) =
     ray.force_raycast_update()
-    let collider = if ray.is_colliding(): ray.get_collider() else: nil
+    let collider = if ray.is_colliding():
+      ray.get_collider().as(Spatial)
+    else:
+      nil
 
     if collider != self.last_collider:
       if self.last_collider != nil:
@@ -35,31 +41,43 @@ gdobj AimTarget of Sprite3D:
 
     if collider != nil:
       let
-        collision_point = ray.get_collision_point()
-        collision_normal = ray.get_collision_normal()
+        local_collision_point = collider.to_local(ray.get_collision_point())
       var
-        target_normal = collision_normal.round()
-        target_point: Vector3
-        block_point: Vector3
-        block_normal: Vector3
-        transform = self.global_transform
+        global_normal = ray.get_collision_normal()
+        local_point: Vector3
 
-      #TODO: This could be better
-      if target_normal == UP or target_normal == DOWN:
-        block_normal = vec3(0.5, 0, 0.5)
-        block_point = (collision_point + block_normal).round()
-        target_point = block_point - block_normal
-      elif target_normal == FORWARD or target_normal == BACK:
-        block_normal = vec3(0.5, 0.5, 0.0)
-        block_point = (collision_point + block_normal).round()
-        target_point =  block_point - block_normal
-      else:
-        block_normal = vec3(0, 0.5, 0.5)
-        block_point = (collision_point + block_normal).round()
-        target_point = block_point - block_normal
-      transform.origin = target_point + collision_normal * 0.01
-      self.global_transform = transform
-      self.rotation_degrees = vec3(target_normal.y, target_normal.x, target_normal.z) * 90
-      collider.trigger("target_move", target_point, target_normal)
+
+      #self.translation = collider.to_global collision_point#collision_point #+ (collision_normal * 0.01)
+
+      let inverse_normal = global_normal.inverse_normalized()
+      let
+        basis = collider.global_transform.basis
+        half = vec3(0.5, 0.5, 0.5)
+        full = vec3(1, 1, 1)
+        fuzzy = vec3(0.05, 0.05, 0.05)
+        local_normal = (basis.xform_inv(global_normal) / collider.scale)
+                             .snapped(half)
+        factor = local_normal.inverse_normalized() * 0.5
+
+      if not local_normal.snapped(fuzzy).is_axis_aligned:
+        # All local normals should be axis aligned because we're dealing with cubes.
+        # If it isn't, we probably got a corner or something.
+        return
+
+      local_point = (local_collision_point - factor).snapped(vec3(1, 1, 1)) + factor
+      global_normal = basis.xform(local_normal) / collider.scale
+
+      self.translation = collider.to_global local_point + (local_normal * 0.01) / collider.scale
+      self.scale = collider.scale
+
+      let
+        align_normal = self.transform.origin + global_normal
+        axis = if local_normal.abs.snapped(fuzzy) != UP: UP else: FORWARD
+      self.look_at(align_normal, axis)
+
+      if (self.last_point, self.last_normal) != (local_point, local_normal):
+        self.last_point = local_point
+        self.last_normal = local_normal
+        collider.trigger("target_move", local_point, local_normal)
 
     self.last_collider = collider
