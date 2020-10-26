2257c23514e660523d2d2e484cedc30c32a8fc04
Scott Wadden <dsrw@me.com>
2020-10-26 15:59:08 -0300

    Project from 0.1 demo

---
+++ app/scripts/bot_1.nim
@@ -0,0 +1,6 @@
+import bot
+run()
+while true:
+  forward 5..10
+  turn_right -45..45
+
---
+++ app/scripts/grid_2.nim
@@ -0,0 +1,9 @@
+import grid
+move:
+  up 5
+  speed = 10
+  scale = 3
+  while true:
+    back 1..5
+    turn_right -45..45
+
