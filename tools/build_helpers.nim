34c6b200c0f2e30326ad3895817cbd3963170611
Scott Wadden <dsrw@me.com>
2020-11-06 15:59:55 -0400

    Removed reset state task

--- tools/build_helpers.nim
+++ tools/build_helpers.nim
@@ -7,13 +7,4 @@ proc generate_api(directory = "godotapi", json = "api.json") =
 
 proc core_count = echo count_processors()
 
-proc reset_state =
-  copy_file "app/scenes/default_level.tscn", "app/scenes/default.tscn"
-  remove_dir "app/state/voxels"
-  for kind in ["grid", "bot"]:
-    for file in walk_files(&"app/scripts/{kind}_*.nim"):
-      remove_file file
-  echo "State deleted"
-
-
-dispatch_multi [generate_api], [core_count], [reset_state]
+dispatch_multi [generate_api], [core_count]
