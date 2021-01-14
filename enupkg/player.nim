26feab668d89b1c8fafaea9a76b3f8be1bdf0253
Scott Wadden <dsrw@me.com>
2021-01-13 21:13:56 -0400

    Don't move camera while editing

--- enupkg/player.nim
+++ enupkg/player.nim
@@ -96,36 +96,37 @@ gdobj Player of KinematicBody:
       self.aim_ray
     else:
       self.world_ray
-
+ 
   method process*(delta: float) {.gdExport.} =
     trace:
-      var transform = self.camera_rig.global_transform
-      transform.origin = self.global_transform.origin + self.position_start
+      if not editing():
+        var transform = self.camera_rig.global_transform
+        transform.origin = self.global_transform.origin + self.position_start
 
-      var look_direction = self.get_look_direction()
+        var look_direction = self.get_look_direction()
 
-      if self.input_relative.length() > 0:
-        self.update_rotation(self.input_relative * sensitivity_mouse)
-        self.input_relative = vec2()
-      elif look_direction.length() > 0:
-        self.update_rotation(look_direction * sensitivity_gamepad * delta)
+        if self.input_relative.length() > 0:
+          self.update_rotation(self.input_relative * sensitivity_mouse)
+          self.input_relative = vec2()
+        elif look_direction.length() > 0:
+          self.update_rotation(look_direction * sensitivity_gamepad * delta)
 
-      var r = self.camera_rig.rotation
-      r.y = wrap(r.y, -PI, PI)
-      self.camera_rig.rotation = r
-      if not get_game().mouse_captured:
-        let
-          mouse_pos = self.get_viewport()
-                          .get_mouse_position() / float get_game()
-                          .shrink
-          cast_from = self.camera.project_ray_origin(mouse_pos)
-          cast_to = self.aim_ray.translation + self.camera.project_ray_normal(mouse_pos) * 100
-        self.world_ray.cast_to = cast_to
-        self.world_ray.translation = cast_from
-        self.aim_target.update(self.world_ray)
-      else:
-        self.aim_ray.cast_to = vec3(0, 0, -100)
-        self.aim_target.update(self.aim_ray)
+        var r = self.camera_rig.rotation
+        r.y = wrap(r.y, -PI, PI)
+        self.camera_rig.rotation = r
+        if not get_game().mouse_captured:
+          let
+            mouse_pos = self.get_viewport()
+                            .get_mouse_position() / float get_game()
+                            .shrink
+            cast_from = self.camera.project_ray_origin(mouse_pos)
+            cast_to = self.aim_ray.translation + self.camera.project_ray_normal(mouse_pos) * 100
+          self.world_ray.cast_to = cast_to
+          self.world_ray.translation = cast_from
+          self.aim_target.update(self.world_ray)
+        else:
+          self.aim_ray.cast_to = vec3(0, 0, -100)
+          self.aim_target.update(self.aim_ray)
 
   method physics_process*(delta: float) =
     trace:
