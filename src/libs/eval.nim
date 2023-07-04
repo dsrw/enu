a4502145a3ad402bf4083c760c79370cd3a46c58
Scott Wadden <scott.wadden@gmail.com>
2023-07-04 13:51:17 -0400

    Switched eval module back to camelCase to make diffing/merging nim changes easier

--- src/libs/eval.nim
+++ src/libs/eval.nim
@@ -10,307 +10,306 @@ export Interpreter, VmArgs, PCtx, PStackFrame, TLineInfo
 # Normal module loading procedure, but makes TPassContextArray a var param
 # so it can be passed to extend_module
 when (NimMajor, NimMinor) >= (1, 7):
-  proc process_module*(graph: ModuleGraph; module: PSym; idgen: IdGenerator;
+  proc processModule*(graph: ModuleGraph; module: PSym; idgen: IdGenerator;
                       stream: PLLStream; a: var TPassContextArray): bool {.discardable.} =
     if graph.stopCompile(): return true
     var
       p: Parser
       s: PLLStream
-      file_idx = module.file_idx
-    prepare_config_notes(graph, module)
-    open_passes(graph, a, module, idgen)
+      fileIdx = module.fileIdx
+    prepareConfigNotes(graph, module)
+    openPasses(graph, a, module, idgen)
     if stream == nil:
-      let filename = to_full_path_consider_dirty(graph.config, file_idx)
-      s = ll_stream_open(filename, fmRead)
+      let filename = toFullPathConsiderDirty(graph.config, fileIdx)
+      s = llStreamOpen(filename, fmRead)
       if s == nil:
-        raw_message(graph.config, err_cannot_open_file, filename.string)
+        rawMessage(graph.config, errCannotOpenFile, filename.string)
         return false
     else:
       s = stream
 
     when defined(nimsuggest):
-      let filename = to_full_path_consider_dirty(graph.config, file_idx).string
-      msgs.set_hash(graph.config, file_idx, $sha1.secure_hash_file(filename))
+      let filename = toFullPathConsiderDirty(graph.config, fileIdx).string
+      msgs.setHash(graph.config, fileIdx, $sha1.secureHashFile(filename))
 
     while true:
-      open_parser(p, file_idx, s, graph.cache, graph.config)
+      openParser(p, fileIdx, s, graph.cache, graph.config)
 
-      if not belongs_to_stdlib(graph, module) or (belongs_to_stdlib(graph, module) and module.name.s == "distros"):
+      if not belongsToStdlib(graph, module) or (belongsToStdlib(graph, module) and module.name.s == "distros"):
         # XXX what about caching? no processing then? what if I change the
         # modules to include between compilation runs? we'd need to track that
         # in ROD files. I think we should enable this feature only
         # for the interactive mode.
         if module.name.s != "nimscriptapi":
-          process_implicits graph, graph.config.implicit_imports, nk_import_stmt, a, module
-          process_implicits graph, graph.config.implicit_includes, nk_include_stmt, a, module
+          processImplicits graph, graph.config.implicitImports, nkImportStmt, a, module
+          processImplicits graph, graph.config.implicitIncludes, nkIncludeStmt, a, module
 
-      check_first_line_indentation(p)
+      checkFirstLineIndentation(p)
       while true:
-        if graph.stop_compile(): break
-        var n = parse_top_level_stmt(p)
-        if n.kind == nk_empty: break
-        if (sf_system_module notin module.flags and
-            ({sf_no_forward, sf_reorder} * module.flags != {} or
-            code_reordering in graph.config.features)):
+        if graph.stopCompile(): break
+        var n = parseTopLevelStmt(p)
+        if n.kind == nkEmpty: break
+        if (sfSystemModule notin module.flags and
+            ({sfNoForward, sfReorder} * module.flags != {} or
+            codeReordering in graph.config.features)):
           # read everything, no streaming possible
-          var sl = new_node_i(nk_stmt_list, n.info)
+          var sl = newNodeI(nkStmtList, n.info)
           sl.add n
           while true:
-            var n = parse_top_level_stmt(p)
-            if n.kind == nk_empty: break
+            var n = parseTopLevelStmt(p)
+            if n.kind == nkEmpty: break
             sl.add n
-          if sf_reorder in module.flags or code_reordering in graph.config.features:
+          if sfReorder in module.flags or codeReordering in graph.config.features:
             sl = reorder(graph, sl, module)
-          discard process_top_level_stmt(graph, sl, a)
+          discard processTopLevelStmt(graph, sl, a)
           break
-        elif n.kind in imperative_code:
+        elif n.kind in imperativeCode:
           # read everything until the next proc declaration etc.
-          var sl = new_node_i(nk_stmt_list, n.info)
+          var sl = newNodeI(nkStmtList, n.info)
           sl.add n
           var rest: PNode = nil
           while true:
-            var n = parse_top_level_stmt(p)
-            if n.kind == nk_empty or n.kind notin imperative_code:
+            var n = parseTopLevelStmt(p)
+            if n.kind == nkEmpty or n.kind notin imperativeCode:
               rest = n
               break
             sl.add n
           #echo "-----\n", sl
-          if not process_top_level_stmt(graph, sl, a): break
+          if not processTopLevelStmt(graph, sl, a): break
           if rest != nil:
             #echo "-----\n", rest
-            if not process_top_level_stmt(graph, rest, a): break
+            if not processTopLevelStmt(graph, rest, a): break
         else:
           #echo "----- single\n", n
-          if not process_top_level_stmt(graph, n, a): break
-      close_parser(p)
-      if s.kind != lls_std_in: break
-    close_passes(graph, a)
-    if graph.config.backend notin {backend_c, backend_cpp, backend_objc}:
+          if not processTopLevelStmt(graph, n, a): break
+      closeParser(p)
+      if s.kind != llsStdIn: break
+    closePasses(graph, a)
+    if graph.config.backend notin {backendC, backendCpp, backendObjc}:
       # We only write rod files here if no C-like backend is active.
       # The C-like backends have been patched to support the IC mechanism.
       # They are responsible for closing the rod files. See `cbackend.nim`.
-      close_rod_file(graph, module)
+      closeRodFile(graph, module)
     result = true
 
 else:
 
-  proc process_module*(graph: ModuleGraph; module: PSym; idgen: IdGenerator;
+  proc processModule*(graph: ModuleGraph; module: PSym; idgen: IdGenerator;
                       stream: PLLStream, a: var TPassContextArray): bool {.discardable.} =
-    if graph.stop_compile(): return true
+    if graph.stopCompile(): return true
     var
       p: Parser
       s: PLLStream
-      file_idx = module.file_idx
-    prepare_config_notes(graph, module)
-    open_passes(graph, a, module, idgen)
+      fileIdx = module.fileIdx
+    prepareConfigNotes(graph, module)
+    openPasses(graph, a, module, idgen)
     if stream == nil:
-      let filename = to_full_path_consider_dirty(graph.config, file_idx)
-      s = ll_stream_open(filename, fm_read)
+      let filename = toFullPathConsiderDirty(graph.config, fileIdx)
+      s = llStreamOpen(filename, fmRead)
       if s == nil:
-        raw_message(graph.config, err_cannot_open_file, filename.string)
+        rawMessage(graph.config, errCannotOpenFile, filename.string)
         return false
     else:
       s = stream
     while true:
-      open_parser(p, file_idx, s, graph.cache, graph.config)
+      openParser(p, fileIdx, s, graph.cache, graph.config)
 
-      if not part_of_stdlib(module) or module.name.s == "distros":
+      if not partOfStdlib(module) or module.name.s == "distros":
         # XXX what about caching? no processing then? what if I change the
         # modules to include between compilation runs? we'd need to track that
         # in ROD files. I think we should enable this feature only
         # for the interactive mode.
         if module.name.s != "nimscriptapi":
-          process_implicits graph, graph.config.implicit_imports, nk_import_stmt,
+          processImplicits graph, graph.config.implicitImports, nkImportStmt,
             a, module
-          process_implicits graph, graph.config.implicit_includes,
-            nk_include_stmt, a, module
+          processImplicits graph, graph.config.implicitIncludes,
+            nkIncludeStmt, a, module
 
       while true:
-        if graph.stop_compile(): break
-        var n = parse_top_level_stmt(p)
-        if n.kind == nk_empty: break
-        if (sf_system_module notin module.flags and
-            ({sf_no_forward, sf_reorder} * module.flags != {} or
-            code_reordering in graph.config.features)):
+        if graph.stopCompile(): break
+        var n = parseTopLevelStmt(p)
+        if n.kind == nkEmpty: break
+        if (sfSystemModule notin module.flags and
+            ({sfNoForward, sfReorder} * module.flags != {} or
+            codeReordering in graph.config.features)):
           # read everything, no streaming possible
-          var sl = new_node_i(nk_stmt_list, n.info)
+          var sl = newNodeI(nkStmtList, n.info)
           sl.add n
           while true:
-            var n = parse_top_level_stmt(p)
-            if n.kind == nk_empty: break
+            var n = parseTopLevelStmt(p)
+            if n.kind == nkEmpty: break
             sl.add n
-          if sf_reorder in module.flags or code_reordering in graph.config.features:
+          if sfReorder in module.flags or codeReordering in graph.config.features:
             sl = reorder(graph, sl, module)
-          discard process_top_level_stmt(graph, sl, a)
+          discard processTopLevelStmt(graph, sl, a)
           break
-        elif n.kind in imperative_code:
+        elif n.kind in imperativeCode:
           # read everything until the next proc declaration etc.
-          var sl = new_node_i(nk_stmt_list, n.info)
+          var sl = newNodeI(nkStmtList, n.info)
           sl.add n
           var rest: PNode = nil
           while true:
-            var n = parse_top_level_stmt(p)
-            if n.kind == nk_empty or n.kind notin imperative_code:
+            var n = parseTopLevelStmt(p)
+            if n.kind == nkEmpty or n.kind notin imperativeCode:
               rest = n
               break
             sl.add n
           #echo "-----\n", sl
-          if not process_top_level_stmt(graph, sl, a): break
+          if not processTopLevelStmt(graph, sl, a): break
           if rest != nil:
             #echo "-----\n", rest
-            if not process_top_level_stmt(graph, rest, a): break
+            if not processTopLevelStmt(graph, rest, a): break
         else:
           #echo "----- single\n", n
-          if not process_top_level_stmt(graph, n, a): break
-      close_parser(p)
-      if s.kind != lls_std_in: break
-    close_passes(graph, a)
-    if graph.config.backend notin {backend_c, backend_cpp, backend_objc}:
+          if not processTopLevelStmt(graph, n, a): break
+      closeParser(p)
+      if s.kind != llsStdIn: break
+    closePasses(graph, a)
+    if graph.config.backend notin {backendC, backendCpp, backendObjc}:
       # We only write rod files here if no C-like backend is active.
       # The C-like backends have been patched to support the IC mechanism.
       # They are responsible for closing the rod files. See `cbackend.nim`.
-      close_rod_file(graph, module)
+      closeRodFile(graph, module)
     result = true
 
 
 # from nimeval. Added moduleName
-proc select_unique_symbol*(i: Interpreter; name: string;
-  sym_kinds: set[TSymKind] = {sk_let, sk_var}; module_name: string): PSym =
+proc selectUniqueSymbol*(i: Interpreter; name: string;
+  symKinds: set[TSymKind] = {skLet, skVar}; moduleName: string): PSym =
   ## Can be used to access a unique symbol of ``name`` and
   ## the given ``symKinds`` filter.
   assert i != nil
-  var module = i.main_module
+  var module = i.mainModule
   for iface in i.graph.ifaces:
-    if iface.module != nil and iface.module.name.s == module_name:
+    if iface.module != nil and iface.module.name.s == moduleName:
       module = iface.module
       break
   assert module != nil, "no module selected"
-  let n = get_ident(i.graph.cache, name)
+  let n = getIdent(i.graph.cache, name)
   var it: ModuleIter
-  var s = init_module_iter(it, i.graph, module, n)
+  var s = initModuleIter(it, i.graph, module, n)
   result = nil
   while s != nil:
-    if s.kind in sym_kinds:
+    if s.kind in symKinds:
       if result == nil: result = s
       else: return nil # ambiguous
-    s = next_module_iter(it, i.graph)
+    s = nextModuleIter(it, i.graph)
 
 # from nimeval. Added moduleName
-proc select_routine*(i: Interpreter; name: string, module_name: string): PSym =
+proc selectRoutine*(i: Interpreter; name: string, module_name: string): PSym =
   ## Selects a declared routine (proc/func/etc) from the main module.
   ## The routine needs to have the export marker ``*``. The only matching
   ## routine is returned and ``nil`` if it is overloaded.
   {.gcsafe.}:
-    result = select_unique_symbol(i, name, {sk_template, sk_macro, sk_func,
-      sk_method, sk_proc, sk_converter}, module_name)
+    result = selectUniqueSymbol(i, name, {skTemplate, skMacro, skFunc,
+      skMethod, skProc, skConverter}, moduleName)
 
-proc reset_module*(i: Interpreter, module_name: string) =
+proc resetModule*(i: Interpreter, moduleName: string) =
   for iface in i.graph.ifaces:
-    if iface.module != nil and iface.module.name.s == module_name:
-      init_str_tables(i.graph, iface.module)
+    if iface.module != nil and iface.module.name.s == moduleName:
+      initStrTables(i.graph, iface.module)
       iface.module.ast = nil
       break
 
-
-proc load_module*(i: Interpreter, file_name, code: string,
+proc loadModule*(i: Interpreter, fileName, code: string,
   a: var TPassContextArray) {.gcsafe.} =
 
   assert i != nil
 
   var module: PSym
-  let module_name = file_name.split_file.name
+  let moduleName = fileName.splitFile.name
   for iface in i.graph.ifaces:
-    if iface.module != nil and iface.module.name.s == module_name:
+    if iface.module != nil and iface.module.name.s == moduleName:
       module = iface.module
       break
 
-  if module.is_nil:
+  if module.isNil:
     {.gcsafe.}:
-      module = i.graph.make_module(file_name)
+      module = i.graph.makeModule(fileName)
 
-  init_str_tables(i.graph, module)
+  initStrTables(i.graph, module)
   module.ast = nil
-  var stream = ll_stream_open(code)
+  var stream = llStreamOpen(code)
 
-  # after some kinds of errors the vm will switch back to em_static_stmt mode,
+  # after some kinds of errors the vm will switch back to emStaticStmt mode,
   # which causes "cannot evaluate at compile time" issues with some variables.
-  # Force things back to em_repl.
-  PCtx(i.graph.vm).mode = em_repl
+  # Force things back to emRepl.
+  PCtx(i.graph.vm).mode = emRepl
   {.gcsafe.}:
-    discard process_module(i.graph, module, i.idgen, stream, a)
+    discard processModule(i.graph, module, i.idgen, stream, a)
 
 # adapted from
 # https://github.com/nim-lang/Nim/blob/version-1-6/compiler/passes.nim#L120
-proc extend_module(graph: ModuleGraph; a: var TPassContextArray, module: PSym;
+proc extendModule(graph: ModuleGraph; a: var TPassContextArray, module: PSym;
     idgen: IdGenerator; stream: PLLStream): bool {.discardable.} =
 
-  if graph.stop_compile(): return true
+  if graph.stopCompile(): return true
   var
     p: Parser
     s = stream
-    file_idx = module.file_idx
+    fileIdx = module.fileIdx
 
   while true:
-    open_parser(p, file_idx, s, graph.cache, graph.config)
+    openParser(p, fileIdx, s, graph.cache, graph.config)
 
     while true:
-      if graph.stop_compile(): break
-      var n = parse_top_level_stmt(p)
-      if n.kind == nk_empty: break
-      if (sf_system_module notin module.flags and
-          ({sf_no_forward, sf_reorder} * module.flags != {} or
-          code_reordering in graph.config.features)):
+      if graph.stopCompile(): break
+      var n = parseTopLevelStmt(p)
+      if n.kind == nkEmpty: break
+      if (sfSystemModule notin module.flags and
+          ({sfNoForward, sfReorder} * module.flags != {} or
+          codeReordering in graph.config.features)):
         # read everything, no streaming possible
-        var sl = new_node_i(nk_stmt_list, n.info)
+        var sl = newNodeI(nkStmtList, n.info)
         sl.add n
         while true:
-          var n = parse_top_level_stmt(p)
-          if n.kind == nk_empty: break
+          var n = parseTopLevelStmt(p)
+          if n.kind == nkEmpty: break
           sl.add n
 
-        discard process_top_level_stmt(graph, sl, a)
+        discard processTopLevelStmt(graph, sl, a)
         break
-      elif n.kind in imperative_code:
+      elif n.kind in imperativeCode:
         # read everything until the next proc declaration etc.
-        var sl = new_node_i(nk_stmt_list, n.info)
+        var sl = newNodeI(nkStmtList, n.info)
         sl.add n
         var rest: PNode = nil
         while true:
-          var n = parse_top_level_stmt(p)
-          if n.kind == nk_empty or n.kind notin imperative_code:
+          var n = parseTopLevelStmt(p)
+          if n.kind == nkEmpty or n.kind notin imperativeCode:
             rest = n
             break
           sl.add n
-        if not process_top_level_stmt(graph, sl, a): break
+        if not processTopLevelStmt(graph, sl, a): break
         if rest != nil:
-          if not process_top_level_stmt(graph, rest, a): break
+          if not processTopLevelStmt(graph, rest, a): break
       else:
-        if not process_top_level_stmt(graph, n, a): break
-    close_parser(p)
-    if s.kind != lls_std_in: break
+        if not processTopLevelStmt(graph, n, a): break
+    closeParser(p)
+    if s.kind != llsStdIn: break
   result = true
 
-proc eval*(i: Interpreter, a: var TPassContextArray, file_name, code: string) =
+proc eval*(i: Interpreter, a: var TPassContextArray, fileName, code: string) =
   ## This can also be used to *reload* the script.
   assert i != nil
   var module: PSym
-  let module_name = file_name.split_file.name
+  let moduleName = fileName.splitFile.name
   for iface in i.graph.ifaces:
-    if iface.module != nil and iface.module.name.s == module_name:
+    if iface.module != nil and iface.module.name.s == moduleName:
       module = iface.module
       break
 
   assert module != nil, "no valid module selected"
-  let s = ll_stream_open(code)
-  extend_module(i.graph, a, module, i.idgen, s)
+  let s = llStreamOpen(code)
+  extendModule(i.graph, a, module, i.idgen, s)
 
 proc config*(i: Interpreter): ConfigRef = i.graph.config
 
-proc register_exit_hook*(i: Interpreter, hook: proc (c: PCtx, pc: int, tos: PStackFrame)) =
-  (PCtx i.graph.vm).exit_hook = hook
+proc registerExitHook*(i: Interpreter, hook: proc (c: PCtx, pc: int, tos: PStackFrame)) =
+  (PCtx i.graph.vm).exitHook = hook
 
-proc register_enter_hook*(i: Interpreter, hook: proc (c: PCtx, pc: int, tos: PStackFrame, instr: TInstr)) =
-  (PCtx i.graph.vm).enter_hook = hook
+proc registerEnterHook*(i: Interpreter, hook: proc (c: PCtx, pc: int, tos: PStackFrame, instr: TInstr)) =
+  (PCtx i.graph.vm).enterHook = hook
 
-proc register_leave_hook*(i: Interpreter, hook: proc (c: PCtx, pc: int, tos: PStackFrame, instr: TInstr)) =
-  (PCtx i.graph.vm).leave_hook = hook
+proc registerLeaveHook*(i: Interpreter, hook: proc (c: PCtx, pc: int, tos: PStackFrame, instr: TInstr)) =
+  (PCtx i.graph.vm).leaveHook = hook
