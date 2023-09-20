b0cb9471b6d5ba9a5529a3ade44f0cb8ead47274
Scott Wadden <scott.wadden@gmail.com>
2023-09-20 19:05:20 -0300

    More json fixes

--- src/models/serializers.nim
+++ src/models/serializers.nim
@@ -83,17 +83,10 @@ proc from_json_hook(self: var Bot, json: JsonNode) =
     self.shared.edits.from_json(json["edits"])
 
 proc `$`(self: Color): string =
-  result = if self == action_colors[eraser]:
-    "\"\""
-  else:
-    for i, color in Colors.enum_fields:
-      if self == action_colors[Colors(i)]:
-        return \"\"{color}\""
-    \"\"{self.to_html_hex}\""
+  $json_utils.to_json(self)
 
 proc `$`(self: VoxelInfo): string  =
   \"[{self.kind.ord}, \"{self.color}\"]"
-
 
 proc `$`(self: Vector3): string =
   \"[{self.x}, {self.y}, {self.z}]"
